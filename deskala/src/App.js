141ba8fa24113180bfec2efe53f000a0f9716506
Saurav Biswas <38159915+saurav49@users.noreply.github.com>
2022-06-08 10:56:47 +0530

    chore
    - cra setup with tailwindcss
    - login and signup screen created

---
+++ deskala/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-sky-500 text-lg font-bold">Deskala - Assignment</h1>
+    </div>
+  );
+}
+
+export default App;
