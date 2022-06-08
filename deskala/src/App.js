a5e524bc88649cef3a45caae02ec3a699754af1d
Saurav Biswas <38159915+saurav49@users.noreply.github.com>
2022-06-08 15:50:58 +0530

    chore
    - router setup
    - components created

--- deskala/src/App.js
+++ deskala/src/App.js
@@ -1,10 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Login, Signup } from "./screens/index";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-sky-500 text-lg font-bold">Deskala - Assignment</h1>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
---
+++ deskala/src/components/PrivateRoute.jsx
@@ -0,0 +1,7 @@
+// import { useLocation, Navigate } from "react-router-dom";
+
+const PrivateRoute = () => {
+  return <div></div>;
+};
+
+export { PrivateRoute };
